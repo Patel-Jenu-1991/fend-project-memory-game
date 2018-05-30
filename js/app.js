a43650409101b28c00a723227efd1639efa99cda
Jaineshkumar Patel <jenu.16@icloud.com>
2018-05-30 03:10:02 -0500

    generate cards dynamically

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@
  * Create a list that holds all of your cards
  */
 
+let cards = [
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-cube",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bomb"
+];
+
+// grab the deck
+const deck = document.querySelector("ul.deck");
 
 /*
  * Display the cards on the page
@@ -10,9 +23,54 @@
  *   - add each card's HTML to the page
  */
 
+// preparing deck..., shuffle cards on the fly
+deck.appendChild(generateCards(shuffle(cards)));
+
+// TODO: write a function to generate the cards
+
+function generateCards(cards) {
+  const fragment = document.createDocumentFragment();
+
+  // iterate over each card in the cards array
+  for (const card of cards) {
+    // create a pair of cards for each card in turn
+    // and append it to the fragment
+    for (let i = 0; i < 2; i++) {
+      fragment.appendChild(createCard(card));
+    }
+  }
+
+  // return the fragment of generated cards
+  return fragment;
+}
+
+// TODO: write a function to generate HTML of a single card
+
+function createCard(card) {
+  // returns card HTML for each given card
+
+  // create the listItem and set its attribute node
+  let listItem = document.createElement("li"),
+      attr = document.createAttribute("class");
+  attr.value = "card";
+  listItem.setAttributeNode(attr);
+
+  // create the icon element and set its attribute node
+  let icon = document.createElement("i"),
+      att = document.createAttribute("class");
+  att.value = `fa ${card}`;
+  icon.setAttributeNode(att);
+
+  // append the icon element to its respective listItem
+  listItem.appendChild(icon);
+
+  // return card HTML as listItem
+  return listItem;
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
